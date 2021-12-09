d957ca8253407afb5e831ebb0686559e9ec0696d
alinaiil <alinail03@mail.ru>
2021-12-09 21:03:14 +0300

    added maxRequestInterval

--- Lab 3/main.c
+++ Lab 3/main.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+void maxRequestInterval(const int *timeList, int interval, int size) {
+    interval--;
+    int left = 0, right = 0;
+    int curLeftDate = 0, curRightDate = 0;
+    int leftDate = 0, rightDate = 0;
+    int maxResult = 0;
+    int counter = 0;
+}
 
 int main() {
     
