5750cfa9fa49cc2c7b9fc4150c815dfea9954886
alinaiil <alinail03@mail.ru>
2021-12-09 21:04:49 +0300

    updated maxRequestInterval

--- Lab 3/main.c
+++ Lab 3/main.c
@@ -9,6 +9,48 @@ void maxRequestInterval(const int *timeList, int interval, int size) {
     int leftDate = 0, rightDate = 0;
     int maxResult = 0;
     int counter = 0;
+	
+	for (int i = 0; i < size; i++) {
+        while (right < size && timeList[right] - timeList[left] <= interval) {
+            counter++;
+            if (timeList[right] > 0) {
+                curRightDate = right;
+            }
+            right++;
+        }
+        if (right == size) {
+            if (maxResult < counter) {
+                maxResult = counter;
+                leftDate = curLeftDate;
+                rightDate = curRightDate;
+            }
+            break;
+        } else {
+            if (maxResult < counter) {
+                maxResult = counter;
+                leftDate = curLeftDate;
+                rightDate = curRightDate;
+            }
+            left++;
+            counter--;
+            curLeftDate = left;
+        }
+    }
+
+    int secLeft, minLeft, hourLeft, dayLeft;
+    secLeft = timeList[leftDate] % 60;
+    minLeft = (timeList[leftDate] / 60) % 60;
+    hourLeft = ((timeList[leftDate] / 60) / 60) % 24;
+    dayLeft = ((timeList[leftDate] / 60) / 60) / 24 + 1;
+    int secRight, minRight, hourRight, dayRight;
+    secRight = timeList[rightDate] % 60;
+    minRight = (timeList[rightDate] / 60) % 60;
+    hourRight = ((timeList[rightDate] / 60) / 60) % 24;
+    dayRight = ((timeList[rightDate] / 60) / 60) / 24 + 1;
+
+    printf("Max number of requests for given interval equals: %d.\n", maxResult);
+    printf("Requests were made from %02d/Jul/1995:%02d:%02d:%02d to %02d/Jul/1995:%02d:%02d:%02d.",
+           dayLeft, hourLeft, minLeft, secLeft, dayRight, hourRight, minRight, secRight);
 }
 
 int main() {
