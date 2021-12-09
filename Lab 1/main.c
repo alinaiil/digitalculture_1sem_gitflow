7661891ca4b6fe94ae2b12179bef4e036d533577
alinaiil <alinail03@mail.ru>
2021-12-09 20:09:49 +0300

    added line counter

--- Lab 1/main.c
+++ Lab 1/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+int lineCounter() {
+    int current = 0;
+    int counter = 1;
+    while (current != EOF) {
+        current = getchar();
+        if (current == '\n') {
+            counter++;
+        }
+    }
+    return counter;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         printf("Wrong number of arguments");
